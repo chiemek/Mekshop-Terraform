345bd3d6520c063d3d6be6c3cca82549b11b3465
chiemek <ekeson1085@gmail.com>
2025-06-13 14:27:29 +0100

    updated the key pair

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "docker" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
-  key_name               = "web-server.pem" 
+  key_name               = "web-server" 
   vpc_security_group_ids = [aws_security_group.common_sg.id, aws_security_group.docker_sg.id]
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_instance" "docker" {
 resource "aws_instance" "prometheus" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
-  key_name               = "web-server.pem" 
+  key_name               = "web-server" 
   vpc_security_group_ids = [aws_security_group.common_sg.id, aws_security_group.prometheus_sg.id]
 
   tags = {
@@ -23,7 +23,7 @@ resource "aws_instance" "prometheus" {
 resource "aws_instance" "grafana" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
-  key_name               = "web-server.pem"  
+  key_name               = "web-server"  
   vpc_security_group_ids = [aws_security_group.common_sg.id, aws_security_group.grafana_sg.id]
 
   tags = {
