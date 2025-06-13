598e20f089a993f847e65406ba5b3b677c4cec7a
chiemek <ekeson1085@gmail.com>
2025-06-13 14:53:43 +0100

    added a bucket for terraform statefile

---
+++ s3_buckets.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "emeka-terraform-state-file"
+
+  versioning {
+    enabled = true
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
